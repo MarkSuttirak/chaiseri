7b4a186cb93773e3222660a7c51b0593ef763761
MarkSuttirak <suttirak.ch@zaviago.com>
2023-09-12 10:02:42 +0700

    Commit Codes

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import './App.scss';
 import HomePage from './pages/home';
+import AboutUs from './pages/about-us';
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<HomePage />} />
+        <Route path="/about-us" element={<AboutUs />} />
       </Routes>
     </BrowserRouter>
   )
--- src/components/header.js
+++ src/components/header.js
@@ -178,27 +178,23 @@ export default function Header() {
                     <Transition
                       as={Fragment}
                       enter="transition ease-out duration-200"
-                      enterFrom="opacity-0 translate-y-1"
-                      enterTo="opacity-100 translate-y-0"
+                      enterFrom="opacity-0 translate-y-3"
+                      enterTo="opacity-100 translate-y-2"
                       leave="transition ease-in duration-150"
-                      leaveFrom="opacity-100 translate-y-0"
-                      leaveTo="opacity-0 translate-y-1"
+                      leaveFrom="opacity-100 translate-y-2"
+                      leaveTo="opacity-0 translate-y-3"
                     >
-                      <Popover.Panel className="absolute z-10 transform px-2 sm:px-0 m-auto flex justify-center">
+                      <Popover.Panel className="absolute z-10 transform m-auto w-[200px]">
                         <div className="overflow-hidden rounded-lg shadow-lg ring-1 ring-black ring-opacity-5">
-                          <div className="relative grid grid-cols-5 bg-white px-5 py-6 sm:p-8">
+                          <div className="relative grid bg-white px-5 py-6">
                             {menusCompany.map((item) => (
                               <a
                                 key={item.name}
                                 href={item.href}
                                 className="-m-3 flex flex-col justify-between rounded-lg p-3 hover:bg-gray-50"
                               >
-                                <div className="flex md:h-full lg:flex-col">
-                                  <div className="ml-4 md:flex md:flex-1 md:flex-col md:justify-between lg:ml-0 lg:mt-4">
-                                    <div>
-                                      <p className="text-base font-medium text-gray-900">{item.name}</p>
-                                    </div>
-                                  </div>
+                                <div className="flex flex-col">
+                                  <p className="text-base font-medium text-gray-900">{item.name}</p>
                                 </div>
                               </a>
                             ))}
@@ -232,11 +228,11 @@ export default function Header() {
                     <Transition
                       as={Fragment}
                       enter="transition ease-out duration-200"
-                      enterFrom="opacity-0 translate-y-1"
-                      enterTo="opacity-100 translate-y-0"
+                      enterFrom="opacity-0 translate-y-3"
+                      enterTo="opacity-100 translate-y-2"
                       leave="transition ease-in duration-150"
-                      leaveFrom="opacity-100 translate-y-0"
-                      leaveTo="opacity-0 translate-y-1"
+                      leaveFrom="opacity-100 translate-y-2"
+                      leaveTo="opacity-0 translate-y-3"
                     >
                       <Popover.Panel className="absolute z-10 w-screen transform px-2 sm:px-0 left-0 w-[90%] m-auto flex justify-center">
                         <div className="overflow-hidden rounded-lg shadow-lg ring-1 ring-black ring-opacity-5 w-[90%]">
